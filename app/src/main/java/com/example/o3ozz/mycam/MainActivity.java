6ec4fbf1e1a7041422c2ffebf12ec03f7484171d
o3ozzvb <ou1112@naver.com>
2017-05-12 01:42:38 +0900

    default

---
+++ app/src/main/java/com/example/o3ozz/mycam/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.o3ozz.mycam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
